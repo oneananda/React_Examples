cb0d8cd0858d2e05656e6b21178e6aaf4ba84f4e
oneananda <anandathirumurugan@gmail.com>
2024-10-20 14:45:28 +0530

    Adding project for useRef

---
+++ UseRef/src/App.tsx
@@ -0,0 +1,37 @@
+import { useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const inputEl = useRef(null);
+
+  const handleFocus = () => {
+    inputEl.current.focus();
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+      <div>
+      <input ref={inputEl} type="text" placeholder="Click the button to focus me" />
+      <button onClick={handleFocus}>Focus the input</button>
+    </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
