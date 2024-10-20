35446c7ccdb1f9535fa6f4c80f2774cabfe23c4c
oneananda <anandathirumurugan@gmail.com>
2024-10-20 14:59:43 +0530

    Adding toggle focus

--- UseRef/src/App.tsx
+++ UseRef/src/App.tsx
@@ -1,37 +1,48 @@
-import { useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const inputEl = useRef(null);
+    const inputEl = useRef(null);
 
-  const handleFocus = () => {
-    inputEl.current.focus();
-  };
+    const buttonEl = useRef(null);
+    // Toggle
+    const [isTextFocussed, setIsTextFocussed] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-      <div>
-      <input ref={inputEl} type="text" placeholder="Click the button to focus me" />
-      <button onClick={handleFocus}>Focus the input</button>
-    </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleFocus = () => {
+        setIsTextFocussed((isTextFocussed) => !isTextFocussed);
+        isTextFocussed ? buttonEl.current.focus() : inputEl.current.focus();
+    };
+
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <div>
+                    <input
+                        ref={inputEl}
+                        type="text"
+                        placeholder="Click the button to focus me"
+                    />
+                    <button ref={buttonEl} onClick={handleFocus}>
+                        {isTextFocussed ? 'Take away the focus' : 'Focus the input'}
+                    </button>
+                </div>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
